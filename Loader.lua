08b9b55126ab5a4a03c2f0c3fe024ad1d92a143b
Phobia <78887483+glitchrer@users.noreply.github.com>
2022-03-14 11:18:26 -0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,14 +1,4 @@
 
-	makefolder("SenselessHub_55884733")
-	writefile("SenselessHub_55884733/!!!DO NOT EDIT ANYTHING HERE!!!","")
-	writefile("SenselessHub_55884733/BackgroundColor","20,20,20")
-	writefile("SenselessHub_55884733/MainColor","28,28,28")
-	writefile("SenselessHub_55884733/AccentColor","0,85,255")
-	writefile("SenselessHub_55884733/RainbowAccentColor","false")
-	writefile("SenselessHub_55884733/OutlineColor","50,50,50")
-	writefile("SenselessHub_55884733/FontColor","255,255,255")
-
-
 
 if identifyexecutor() == "ScriptWare" then
 	loadstring(game:HttpGet('https://raw.githubusercontent.com/glitchrer/Senseless-Hub/main/ScriptWaretoSynapse.lua'))()
