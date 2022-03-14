bfa14e782d0cc1de94765e6f2b790a93fcbe0d62
Phobia <78887483+TheMightySource@users.noreply.github.com>
2022-03-14 16:36:03 -0700

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -4,6 +4,6 @@ local SupportedGames = {6137522359}
 
 for i,v in pairs(SupportedGames) do
     if v == game.PlaceId then
-    loadstring(game:HttpGet(('https://raw.githubusercontent.com/glitchrer/Senseless-Hub/main/Games/'..v),true))()
+    loadstring(game:HttpGet(('https://raw.githubusercontent.com/TheMightySource/Senseless-Hub/main/Games/'..v),true))()
     end
     end
