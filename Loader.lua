c1936f8b6423fa7ed432a6f03eff80f9323e9dd6
Phobia <78887483+glitchrer@users.noreply.github.com>
2022-03-14 09:56:26 -0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,3 +1,11 @@
+if isfolder("SenselessHub_55884733") then
+
+	else
+	makefolder("SenselessHub_55884733")
+	writefile("SenselessHub_55884733/DO NOT EDIT ANYTHING HERE","")
+end
+
+
 if identifyexecutor() == "ScriptWare" then
 	loadstring(game:HttpGet('https://raw.githubusercontent.com/glitchrer/Senseless-Hub/main/ScriptWaretoSynapse.lua'))()
 	wait(1)
