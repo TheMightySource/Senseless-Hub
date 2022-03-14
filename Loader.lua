41f4193ba511fe98a9197f56d1a1809536ace6f6
Phobia <78887483+glitchrer@users.noreply.github.com>
2022-03-13 18:29:32 -0700

    Create Loader.lua

---
+++ Loader.lua
@@ -0,0 +1,7 @@
+if identifyexecutor() == "ScriptWare" then
+	loadstring(game:HttpGet('https://raw.githubusercontent.com/glitchrer/Senseless-Hub/main/ScriptWaretoSynapse.lua'))()
+	wait(1)
+	else
+  
+	
+end
