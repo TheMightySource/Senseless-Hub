7a586433dccd517ca632ed1d21e53e7a20c5a19c
Phobia <78887483+TheMightySource@users.noreply.github.com>
2022-03-15 18:50:30 -0700

    Create Script.lua

---
+++ Script.lua
@@ -0,0 +1,9 @@
+
+_G.SenselessHub_Theme = Light -- Light, Dark, Mocha, Aqua, Jester
+_G.SenselessHub_Style = 2
+_G.SenselessHub_SizeX = 500
+_G.SenselessHub_SizeY = 350
+_G.SenselessHub_ColorOverrideMainFrame = 235,235,235
+-- These are default settings, for more information visit materiallua.gq
+
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/TheMightySource/Senseless-Hub/main/Loader.lua'),true))()
