ac9ba74542ba1f14b8825d9de168f0f1b733460b
Phobia <78887483+TheMightySource@users.noreply.github.com>
2022-03-24 15:10:59 -0700

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,7 +1,5 @@
 if game.PlaceId == 6137522359 then
     loadstring(game:HttpGet('https://raw.githubusercontent.com/TheMightySource/Senseless-Hub/main/Games/'..game.PlaceId))()
-    end
-
-if game.PlaceId == 155615604 then
+    elseif game.PlaceId == 155615604 then
     loadstring(game:HttpGet('https://raw.githubusercontent.com/TheMightySource/Senseless-Hub/main/Games/'..game.PlaceId))()
     end
